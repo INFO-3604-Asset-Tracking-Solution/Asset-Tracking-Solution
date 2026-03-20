48938d945cad3738d9c57a1d392e7b6cf3d80371
haleyskeete3 <haleyskeete@gmail.com>
2026-03-20 16:56:03 -0400

    assetassignment

--- flaskmvc-main/flaskmvc-main/App/models/assetassignment.py
+++ flaskmvc-main/flaskmvc-main/App/models/assetassignment.py
@@ -2,15 +2,18 @@ from App.database import db
 from datetime import datetime
 
 class AssetAssignment(db.Model):
+    __tablename__ = "asset_assignment"
+
     assignment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     asset_id = db.Column(db.String(50), db.ForeignKey('asset.asset_id'), nullable=False)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
-
     floor_id = db.Column(db.String(50), db.ForeignKey('floor.floor_id'), nullable=False)
-    assignment_date = db.Column(
-        db.DateTime, nullable=False, default=datetime.utcnow
-    )
+    assignment_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     return_date = db.Column(db.DateTime, nullable=True)
+
+    asset = db.relationship('Asset', backref='assignments', lazy=True)
+    employee = db.relationship('Employee', backref='assignments', lazy=True)
+    room = db.relationship('Room', backref='assignments', lazy=True)
 
     def __init__(self, asset_id, employee_id, floor_id, assignment_date=None, return_date=None):
         self.asset_id = asset_id
@@ -28,9 +31,9 @@ class AssetAssignment(db.Model):
             'assignment_date': self.assignment_date,
             'return_date': self.return_date
         }
-    
+
     def __repr__(self):
         return f"<AssetAssignment {self.assignment_id}>"
-    
+
     def __str__(self):
         return f'Assignment {self.assignment_id} (Asset: {self.asset_id}, Employee: {self.employee_id}, Floor: {self.floor_id})'
